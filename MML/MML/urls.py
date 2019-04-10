fab63169331631fd585fe0bf4d41ef6bcec48b15
mikiribe14 <alcarras.miquel.ribes@gmail.com>
2019-04-10 21:47:36 +0200

    Button fill database from itunes api

--- MML/MML/urls.py
+++ MML/MML/urls.py
@@ -29,6 +29,8 @@ urlpatterns = [
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^profile/(?P<username>\w+)/$',views.profile,name='profile'),
     url(r'^generate/$',views.generate,name='generate'),
+#    url(r'^generate/$',views.generateSongs,name='generateSongs'),
+
 
     #Are they necessary?
     url(r'^groups/$', views.GroupListView.as_view(), name='Group'),
